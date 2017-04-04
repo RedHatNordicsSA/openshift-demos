dd75ad847339ac804fa369e1fdf9c9bbf370e33d
Magnus Glantz <sudo@redhat.com>
2017-04-04 13:22:09 +0200

    Initial commit, HPA demo

--- source/hpa/install.sh
+++ source/hpa/install.sh
@@ -5,5 +5,5 @@
 oc new-project demo-hpa
 oc new-app --template=openshift-tasks
 oc autoscale dc/tasks --min 1 --max 3 --cpu-percent=60
-oc create -f https://raw.githubusercontent.com/mglantz/openshift-demos/master/source/hpa/limits.json
+oc create -f https://raw.githubusercontent.com/mglantz/openshift-demos/master/source/hpa/limit.json
 oc rollout latest dc/tasks
