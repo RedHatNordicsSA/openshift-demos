3c8940bc4f3af5d6aed5203c2d912cfaa469fce3
Magnus Glantz <sudo@redhat.com>
2017-04-11 13:02:55 +0200

    NodeJS instead of PHP

--- source/abtesting/run.sh
+++ source/abtesting/run.sh
@@ -12,12 +12,12 @@ sleep 1
 oc project demo-abtesting
 
 echo
-if oc get pods|egrep '(php56|php70)'|grep Running|grep "1/1"|wc -l|grep 2 >/dev/null; then
+if oc get pods|egrep '(nodejs010|nodejs40)'|grep Running|grep "1/1"|wc -l|grep 2 >/dev/null; then
 	echo "$(date): Applications are up. Displaying applications in project demo-abtesting:"
-	oc get pods|egrep '(php56|php70)'|grep Running|grep "1/1"
+	oc get pods|egrep '(nodejs010|nodejs40)'|grep Running|grep "1/1"
 	echo
 	echo "$(date): Displaying AB route:"
-	oc get route|grep ab-php
+	oc get route|grep ab-nodejs
 	echo
 
 else
@@ -25,19 +25,19 @@ else
 	exit 1
 fi
 
-oc scale --replicas=1 dc php70
-oc scale --replicas=1 dc php56
+oc scale --replicas=1 dc nodejs40
+oc scale --replicas=1 dc nodejs010
 
-oc set route-backends ab-php php70=10 php56=90
-ABROUTE=$(oc get route|grep ab-php|awk '{ print $2 }')
+oc set route-backends ab-php nodejs40=10 nodejs010=90
+ABROUTE=$(oc get route|grep ab-nodejs|awk '{ print $2 }')
 
 echo
-echo "$(date): Making 10 http requests to the load balanced AB route. 1 out of 10 requests will go to the pod running PHP 7.0."
+echo "$(date): Making 10 http requests to the load balanced AB route. 1 out of 10 requests will go to the pod running NodeJS 4.0."
 sleep 1
 
 echo
 echo "$(date): Displaying AB route:"
-oc get route|grep ab-php
+oc get route|grep ab-nodejs
 sleep 1
 
 echo
@@ -53,13 +53,13 @@ echo
 read -p "Press enter to continue demo." GO
 
 echo
-echo "$(date): Changing load balancing distribution to send 1 out of 5 requests to the pod running PHP 7.0."
-oc set route-backends ab-php php70=20 php56=80
+echo "$(date): Changing load balancing distribution to send 1 out of 5 requests to the pod running NodeJS 4.0."
+oc set route-backends ab-nodejs nodejs40=20 nodejs010=80
 sleep 1
 
 echo
 echo "$(date): Displaying AB route:"
-oc get route|grep ab-php
+oc get route|grep ab-nodejs
 sleep 1
 
 echo
@@ -76,13 +76,13 @@ echo
 read -p "Press enter to continue demo." GO
 
 echo
-echo "$(date): Changing load balancing distribution to send 1 out of 2 requests to the pod running PHP 7.0."
+echo "$(date): Changing load balancing distribution to send 1 out of 2 requests to the pod running NodeJS 4.0."
 
-oc set route-backends ab-php php70=50 php56=50
+oc set route-backends ab-nodejs nodejs40=50 nodejs010=50
 
 echo
 echo "$(date): Displaying AB route:"
-oc get route|grep ab-php
+oc get route|grep ab-nodejs
 sleep 1
 
 echo
@@ -99,19 +99,19 @@ echo
 read -p "Press enter to continue demo." GO
 
 echo
-echo "$(date): Changing load balancing distribution to send all requests to the pod running PHP 7.0."
+echo "$(date): Changing load balancing distribution to send all requests to the pod running NodeJS 4.0."
 
-oc set route-backends ab-php php70=100 php56=0
+oc set route-backends ab-nodejs nodejs40=100 nodejs010=0
 
 echo
 echo "$(date): Displaying AB route:"
-oc get route|grep ab-php
+oc get route|grep ab-nodejs
 sleep 1
 
-echo "$(date): Scaling pod that runs PHP 7.0 to 2 pods."
+echo "$(date): Scaling pod that runs Nodejs 4.0 to 2 pods."
 
-oc scale --replicas=2 dc php70
-sleep 10
+oc scale --replicas=2 dc nodejs40
+sleep 15
 
 echo "$(date): Displaying running pods."
 oc get pods|grep Running
@@ -124,9 +124,9 @@ for i in {1..10}; do
 done
 
 echo
-echo "$(date): Scaling number of pods running PHP 5.6 to 0."
+echo "$(date): Scaling number of pods running NodeJS 0.10 to 0."
 
-oc scale --replicas=0 dc php56
+oc scale --replicas=0 dc nodejs010
 
 sleep 5
 echo "$(date): Displaying pods."
