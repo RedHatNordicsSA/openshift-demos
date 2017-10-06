5da996dffc2b9985f01cabca5aa97dbdf0b5f16a
Johannes Brännström <jbrannst@redhat.com>
2017-10-06 17:01:26 +0200

    import blue-green installer from main repo

---
+++ source/blue-green/install.sh
@@ -0,0 +1,4 @@
+curl -O https://raw.githubusercontent.com/jbrannst/openshift-cd-demo/ocp-3.5/setup.sh
+curl -O https://raw.githubusercontent.com/jbrannst/openshift-cd-demo/ocp-3.5/cicd-template.yaml
+chmod u+x setup.sh
+./setup.sh
